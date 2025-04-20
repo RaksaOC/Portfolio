d00f2d9f53a6c2e40f12bd109b14a8f31ef254d4
Chanraksa Ory <OCraksa@gmail.com>
2025-04-20 17:07:02 +0700

    trying to fix snappy mobile scroll

--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ import "aos/dist/aos.css"; // Import AOS CSS
 createApp(App).mount("#app");
 
 AOS.init({
-  duration: 1000, // Adjust timing
-  easing: "ease-in-out", // Smooth effect
-  once: false, // Keep animations on scroll
-//   offset: 400, // Adjust when animations start
+  duration: 800,
+  easing: "ease-in-out",
+  once: true,
+  mirror: false,
+  // offset: 100,
 });
+
 
