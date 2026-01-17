ed637b89140cc9eca21db3f1cfb8480610c394a9
Chanraksa Ory <OCraksa@gmail.com>
2026-01-17 23:37:20 +0700

    feat: SEO

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import {createApp} from "vue";
+import { createApp } from "vue";
 import "./global.css";
 import App from "./App.vue";
-import {onMounted} from "vue";
 import AOS from "aos";
 import "aos/dist/aos.css"; // Import AOS CSS
 
@@ -10,10 +9,8 @@ createApp(App).mount("#app");
 const isMobile = window.innerWidth <= 768;
 
 AOS.init({
-    duration: isMobile ? 1200 : 800,
-    easing: isMobile ? "ease-in-out-back" : "ease-in-out-sine",
-    once: isMobile,
-    mirror: false
+  duration: isMobile ? 1200 : 800,
+  easing: isMobile ? "ease-in-out-back" : "ease-in-out-sine",
+  once: isMobile,
+  mirror: false,
 });
-
-
